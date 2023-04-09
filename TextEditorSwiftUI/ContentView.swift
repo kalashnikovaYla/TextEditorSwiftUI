34fef96284713c2540b38ed124a41232b368c711
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 22:11:08 +0500

    Initial

--- TextEditorSwiftUI/ContentView.swift
+++ TextEditorSwiftUI/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = ""
+    @State var totalChars = 0
+    @State var lastText = ""
+    
+    init() {
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            ProgressView("Character \(totalChars) / 150 ", value: Double(totalChars), total: 150)
+                .frame(width: 300)
+                .padding()
+                .foregroundColor(.white)
+                .tint(.black)
+            
+            TextEditor(text: $text)
+                .padding()
+                
+                .background(.yellow)
+                .scrollContentBackground(.hidden)
+                .foregroundColor(.purple)
+                .font(.title)
+                .frame(width: 370, height: 300)
+                .cornerRadius(24)
+                .onChange(of: text) { newValue in
+                    totalChars = text.count
+                    if totalChars <= 150 {
+                        lastText = text
+                    } else {
+                        text = lastText
+                    }
+                }
+               
+            Spacer()
         }
         .padding()
+        .background(.purple)
+        .onTapGesture {
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        }
     }
 }
 
