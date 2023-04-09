2855446f64abd14c22d72165bf69ff54dfb415b1
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-09 20:52:46 +0500

    Initial Commit

---
+++ TextEditorSwiftUI/TextEditorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorSwiftUIApp.swift
+//  TextEditorSwiftUI
+//
+//  Created by sss on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
